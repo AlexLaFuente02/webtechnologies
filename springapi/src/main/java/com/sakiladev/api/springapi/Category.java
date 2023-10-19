acaa98aa1adbc4056a67deb06d063a304805ded1
AlexLaFuente02 <alexlafuentelp@gmail.com>
2023-10-19 11:45:55 -0400

    Entidad modificado y port

--- springapi/src/main/java/com/sakiladev/api/springapi/Category.java
+++ springapi/src/main/java/com/sakiladev/api/springapi/Category.java
@@ -7,8 +7,10 @@ import jakarta.persistence.*;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
     private Long categoryId;
 
+    @Column(name = "name")
     private String name;
 
     // Getters and setters
